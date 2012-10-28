57c62d3cc46c74f61f05d3b5c5d1f40dc7123b1a
Darko Grozdanovski <darkogrozdanovski@gmail.com>
2012-10-28 16:59:46 +0100

    working tests

--- ruby/spec/spec_helper.rb
+++ ruby/spec/spec_helper.rb
@@ -4,19 +4,10 @@ require_relative '../lib/chute'
 # require(File.expand_path('../../lib/chute', __FILE__))
 
 #dependencies
+
 require 'minitest/autorun'
 require 'webmock/minitest'
 require 'vcr'
-require 'turn'
-
-Turn.config do |c|
-  # :outline  - turn's original case/test outline mode [default]
-  c.format  = :outline
-  # turn on invoke/execute tracing, enable full backtrace
-  c.trace   = true
-  # use humanized test names (works only with :outline format)
-  c.natural = true
-end
 
 #VCR config
 VCR.config do |c|
