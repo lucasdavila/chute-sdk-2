23accfbb17fecfb0ed4ad1ce8574ed3ea0f4bff9
Darko Grozdanovski <darkogrozdanovski@gmail.com>
2012-10-30 05:09:48 -0700

    client connection, Albums Model assets model

--- ruby/lib/chute/v2/assets.rb
+++ ruby/lib/chute/v2/assets.rb
@@ -4,42 +4,42 @@ module Chute
       class << self
         # Asset Listing
         def all
-          Chute::Request.get("/v2/assets")
+          Chute::Client.get("/v2/assets")
         end
 
         # Asset Geo Search
         def geo_search(lat_lng, radius)
-          Chute::Request.get("/v2/assets/geo/#{lat_lng}/#{radius}")
+          Chute::Client.get("/v2/assets/geo/#{lat_lng}/#{radius}")
         end
 
         # Asset Details
         def find(id)
-          Chute::Request.get("/v2/assets/#{id}")
+          Chute::Client.get("/v2/assets/#{id}")
         end
 
         # Asset Exif
         def exif(id)
-          Chute::Request.get("/v2/assets/#{id}/exif")
+          Chute::Client.get("/v2/assets/#{id}/exif")
         end
 
         # Asset Geo
         def geo(id)
-          Chute::Request.get("/v2/assets/#{id}/geo")
+          Chute::Client.get("/v2/assets/#{id}/geo")
         end
 
         # Asset Import
         def create(data)
-          Chute::Request.post("/v2/assets/_upload", :urls=> urls)
+          Chute::Client.post("/v2/assets/_upload", :urls=> urls)
         end
 
         # Album Update
         def update(id, data)
-          Chute::Request.put("/v2/assets/#{id}", data)
+          Chute::Client.put("/v2/assets/#{id}", data)
         end
 
         # Album Delete
         def delete(id)
-          Chute::Request.delete("/v2/assets/#{id}")
+          Chute::Client.delete("/v2/assets/#{id}")
         end
       end
     end
