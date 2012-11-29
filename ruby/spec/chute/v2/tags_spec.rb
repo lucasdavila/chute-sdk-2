26006c68decf2f6ce27ba9b8ffa7570b745415ee
Darko Grozdanovski <darkogrozdanovski@gmail.com>
2012-11-29 15:51:53 +0100

    added tags and specs

--- ruby/spec/chute/v2/tags_spec.rb
+++ ruby/spec/chute/v2/tags_spec.rb
@@ -37,33 +37,6 @@ describe Chute::V2::Tags do
   end
 
 
-  describe "asset tags" do
-
-    before do
-      VCR.insert_cassette 'tags/tags_assets', :record => :new_episodes
-    end
-    after do
-      VCR.eject_cassette
-    end
-    it "should be able to add asset tags" do
-      response = Chute::V2::Tags.add_asset_tags(asset_id.data[0].id, {:tags => ["TAG","AnotherTag"]})
-      response.success?.should == true
-    end
-
-
-    it "should list all asset tags" do
-      response = Chute::V2::Tags.list_asset_tags(asset_id.data[0].id)
-      response.success?.should == true
-      response.to_pretty_json
-    end
-
-    it "should replace all existing asset tags" do
-      response = Chute::V2::Tags.update_asset_tags(asset_id.data[0].id,{:tags => ["Replaced TAG1","Replaced Tag2"]} )
-      response.success?.should == true
-    end
-
-  end
-
   describe "album tags" do
 
     before do
@@ -73,12 +46,49 @@ describe Chute::V2::Tags do
       VCR.eject_cassette
     end
 
+    it "should be able to add album tags" do
+      response = Chute::V2::Tags.add_album_tags(album_id.id, {:tags => ["TAG","AnotherTag"]})
+      response.success?.should == true
+    end
+
     it "should list all album tags" do
-      #album_id.to_pretty_json
-      #response = Chute::V2::Tags.list_album_tags(album_id)
-     # response.to_pretty_json
+      response = Chute::V2::Tags.list_album_tags(album_id.id)
+      response.success?.should == true
+    end
+
+    it "should replace all existing album tags" do
+      response = Chute::V2::Tags.update_album_tags(album_id.data.id,{:tags => ["Replaced TAG1","Replaced Tag2"]} )
+      response.success?.should == true
     end
 
   end
+
+  describe "asset tags" do
+
+    before do
+      VCR.insert_cassette 'tags/tags_assets', :record => :new_episodes
+    end
+    after do
+      VCR.eject_cassette
+    end
+
+    it "should be able to add asset tags" do
+      response = Chute::V2::Tags.add_asset_tags(asset_id.data[0].id, {:tags => ["TAG","AnotherTag"]})
+      response.success?.should == true
+    end
+
+    it "should list all asset tags" do
+      response = Chute::V2::Tags.list_asset_tags(asset_id.data[0].id)
+      response.success?.should == true
+    end
+
+    it "should replace all existing asset tags" do
+      response = Chute::V2::Tags.update_asset_tags(asset_id.data[0].id,{:tags => ["Replaced TAG1","Replaced Tag2"]} )
+      response.success?.should == true
+    end
+
+  end
+
+
 
 end
